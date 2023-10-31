1b22fbe6f12d8820458823aaff8a9156cead718c
Javi1f <javifemeza76@gmail.com>
2023-10-31 14:39:36 -0500

    Proyect creation
    The spring proyect has been created to stard developing

---
+++ src/main/java/co/edu/unbosque/RiskGameProyect/RiskGameProyectApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.RiskGameProyect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiskGameProyectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiskGameProyectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/unbosque/RiskGameProyect/RiskGameProyectApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.RiskGameProyect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiskGameProyectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
